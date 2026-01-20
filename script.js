756e409add0b056c205effd4637a048c4431e800
Aditya Singh <adhacks@Adityas-MacBook-Air.local>
2026-01-20 18:51:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// var a;    declaration
+// var a = 12;   declaration and initialization
+// let a;
+// let a=12;
+// const a =10;
+
+// var a=12;
+//window mein add hota hai
+//function scoped hota hai
+//aap firse declare kar sakte hai same name se and error nhi aayega
+
+const discount = 10;
+

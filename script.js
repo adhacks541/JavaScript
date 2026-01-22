3caad2004d3fb3b80e29843cd0489753d760ee8f
Aditya Singh <adhacks@Adityas-MacBook-Air.local>
2026-01-22 23:46:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,21 @@
 // block scope -- Access in block
 // functional scope -- Access in function
 
-//learned more things related to js
-//will update that tomorrow 
+//Reassignment , Redeclaration
+// var a = 12;
+// a = 34;
+// let b = 43;
+// b = 65;
 
+//Temporal Dead Zone -- utna area jitne mein js ko pata to hai ki variable exist karta hai par woh aapko value nhi de sakta
+// console.log(a);
+// let a = 12;
 
+//Hoisting impact per type
+//hoisting -> ek variable ko jab js mein banate hai toh woh variable do hisso mein toot jaata hai and uska declare part upar chala jaata hai and uska initialization part reh jata hai
+// console.log(a);
+// var a = undefined;
+// a = 12;
+// var --> hoist --> undefined
+// let --> hoist --> x
+// const --> hoist --> x
